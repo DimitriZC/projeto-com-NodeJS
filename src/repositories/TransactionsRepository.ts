0faeb402e4331f98b377efc3c5cbbba7ef62ad99
Dimitri <dimitrizuave14@gmail.com>
2020-04-15 20:43:55 -0300

    tests passed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions.reduce((total, transaction) => {
+      const { value } = transaction;
+
+      return transaction.type === 'income' ? total + value : total;
+    }, 0);
+
+    const totalOutcome = this.transactions.reduce((total, transaction) => {
+      const { value } = transaction;
+
+      return transaction.type === 'outcome' ? total + value : total;
+    }, 0);
+
+    const total = totalIncome - totalOutcome;
+
+    const balance: Balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
